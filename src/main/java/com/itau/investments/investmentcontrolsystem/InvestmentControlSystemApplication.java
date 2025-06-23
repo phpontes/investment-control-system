6266543105206a14c23edfdbc2dce21ef8070480
phpontes <ppontes95@hotmail.com>
2025-06-23 10:40:10 -0300

    Projeto base iniciado: estrutura Spring Boot gerada

---
+++ src/main/java/com/itau/investments/investmentcontrolsystem/InvestmentControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.itau.investments.investmentcontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/investments/investmentcontrolsystem/InvestmentControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.investments.investmentcontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
